6b08b41961116b20ac95c26f074ebc147fcc69d5
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-01-24 16:57:18 +0545

    Frontpage Integration

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+    
+    namespace App\Http\Controllers;
+    
+    use App\Models\Tag;
+    use App\Models\Post;
+    use App\Models\Category;
+    
+    class WelcomeController extends Controller
+    {
+        public function index()
+        {
+            return view('welcome', [
+                'categories' => Category::all(),
+                'tags' => Tag::all(),
+                'posts' => Post::all(),
+            ]);
+        }
+    }
